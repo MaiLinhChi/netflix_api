7a8bda6ca39fe50f698983d164733f5363ab19b7
MaiLinhChi <mailinhchi22082002@gmail.com>
2023-12-07 07:53:13 +0700

    validate

--- validation/users.js
+++ validation/users.js
@@ -16,12 +16,11 @@ const update = Joi.object({
   name: Joi.string().min(2).optional(),
   email: Joi.string().email().optional(),
   phone: Joi.string().min(7).max(11).optional(),
-  job: Joi.string().min(3).max(15).optional(),
+  job: Joi.string().min(2).optional(),
   address: Joi.string().min(3).optional(),
   profilePicture: Joi.string().optional(),
   role: Joi.string().valid("guest", "manager", "admin").optional(),
   updatedAt: Joi.string().required(),
-  password: Joi.string().min(6).max(30).optional(),
 });
 
 module.exports = {
